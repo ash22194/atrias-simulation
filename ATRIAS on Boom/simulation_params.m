27a3aacfd50ecda534629db900a65c6c2ef2f438
williamcmartin <wmartin@cmu.edu>
2016-09-22 21:04:10 -0400

    Updated the boom constrained simulation to match hardware.
    Boom simulation is now in z-up frame. IMU orientations and accelerations
    are now included. Included a compiled deadbeat running controller to
    demonstrate simulator.

--- ATRIAS on Boom/simulation_params.m
+++ ATRIAS on Boom/simulation_params.m
@@ -1,12 +1,32 @@
+sample_freq = 1000;
+update_freq = sample_freq;
+sample_time = 1/sample_freq;
+
 %% Timings
-t_end = 30;  
-output_sample_time = 1/120;
+sm.t_end = 30;  
+sm.output_sample_time = 1/120;
 
 % Sensors
-sample_freq = 1000;
-sample_time = 1/sample_freq;
-lpf_damping = sqrt(2)/2; % Butterworth filter damping
-fcut_velocity = 100*(2*pi); % Cutoff frequency for velocities
-B1_lpf_velocity = -2*exp(-lpf_damping*fcut_velocity*sample_time)*cos(fcut_velocity*sample_time*sqrt(1-lpf_damping^2));
-B2_lpf_velocity = exp(-2*lpf_damping*fcut_velocity*sample_time);
-A_lpf_velocity = 1 + B1_lpf_velocity + B2_lpf_velocity;
+sm.lpf_damping = sqrt(2)/2; % Butterworth filter damping
+sm.fcut_velocity = 100*(2*pi); % Cutoff frequency for velocities
+sm.B1_lpf_velocity = -2*exp(-sm.lpf_damping*sm.fcut_velocity*sample_time)*cos(sm.fcut_velocity*sample_time*sqrt(1-sm.lpf_damping^2));
+sm.B2_lpf_velocity = exp(-2*sm.lpf_damping*sm.fcut_velocity*sample_time);
+sm.A_lpf_velocity = 1 + sm.B1_lpf_velocity + sm.B2_lpf_velocity;
+
+%% Noise
+sm.sensor_noise_variance = (2e-4)^2*ones(13,1);
+
+%% SEA Control
+kp_sea_torque = 10;
+kd_sea_torque = 0.9*2*sqrt(kp_sea_torque/3500*3.75);
+
+%-- Low Pass filter for torque derivative
+fcut_dtau = 80*(2*pi); % Hz, Low pass filter cutoff frequency when calculating acceleration from velocity
+lpf_damping = sqrt(2)/2; % butterworth damping ratio
+B1_lpf_dtau = -2*exp(-lpf_damping*fcut_dtau*sample_time)*cos(fcut_dtau*sample_time*sqrt(1-lpf_damping^2));
+B2_lpf_dtau = exp(-2*lpf_damping*fcut_dtau*sample_time);
+A_lpf_dtau = 1 + B1_lpf_dtau + B2_lpf_dtau;
+
+%% Extra
+kp_motor = 5000;
+kd_motor = 0.9*2*sqrt(3.75*kp_motor);
